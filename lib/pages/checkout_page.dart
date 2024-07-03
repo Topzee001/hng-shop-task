dde523f2846d4951fe63ad1eaedfeb0362b4ef09
Ibrahim <ibrahimsakariyah@gmail.com>
2024-07-03 16:15:10 -0600

    update

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -47,77 +47,84 @@ class _MyCheckoutPageState extends State<MyCheckoutPage> {
           centerTitle: true,
           title: const Text('Checkout'),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: widget.cartModel.cartItems.length,
-                itemBuilder: (context, index) {
-                  final item = widget.cartModel.cartItems[index];
-                  return ListTile(
-                    leading: Image.asset(
-                      item.product.imageUrl,
-                      height: 140,
-                    ),
-                    title: Text(item.product.name),
-                    subtitle: Text(
-                      '\$${item.product.price.toStringAsFixed(2)}',
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.remove_shopping_cart),
-                      onPressed: () =>
-                          widget.cartModel.removeItemFromCart(item.product.id),
-                    ),
-                  );
-                },
-              ),
-            ),
-            GestureDetector(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(
-                      color: Colors.purple,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(12),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Checkout',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Colors.white,
-                      )
-                    ],
-                  ),
+        body: widget.cartModel.cartItems.isEmpty
+            ? const Center(
+                child: Text(
+                  'Item not added to cart',
+                  style: TextStyle(fontSize: 18),
                 ),
+              )
+            : Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.cartModel.cartItems.length,
+                      itemBuilder: (context, index) {
+                        final item = widget.cartModel.cartItems[index];
+                        return ListTile(
+                          leading: Image.asset(
+                            item.product.imageUrl,
+                            height: 140,
+                          ),
+                          title: Text(item.product.name),
+                          subtitle: Text(
+                            '\$${item.product.price.toStringAsFixed(2)}',
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.remove_shopping_cart),
+                            onPressed: () => widget.cartModel
+                                .removeItemFromCart(item.product.id),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  if (widget.cartModel.cartItems.isNotEmpty)
+                    GestureDetector(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.purple,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Checkout',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        widget.cartModel.clearCart();
+                        Navigator.of(context).pushAndRemoveUntil(
+                          MaterialPageRoute(
+                              builder: (context) => const MyOrderSuccess()),
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                    ),
+                ],
               ),
-              onTap: () {
-                widget.cartModel.clearCart();
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(
-                      builder: (context) => const MyOrderSuccess()),
-                  (Route<dynamic> route) => false,
-                );
-                //.then((_) {
-
-                //});
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
